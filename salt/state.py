ec5a11a59a9b52e381694a1e968ef899b866f83d
Cristian Hotea <cristian.hotea@ni.com>
2019-11-14 11:29:10 +0200

    Add the jid to cdata kwargs in order to be passed to modules
    We need the jid in the execution modules ran from state
    to be able to publish the software installation progress
    information.
    
    Signed-off-by: Cristian Hotea <cristian.hotea@ni.com>

--- salt/state.py
+++ salt/state.py
@@ -2382,7 +2382,7 @@ class State:
             initial_ret={"full": state_func_name},
             expected_extra_kws=STATE_INTERNAL_KEYWORDS,
         )
-
+        cdata["kwargs"]["__pub_jid"] = self.jid
         inject_globals = {
             # Pass a copy of the running dictionary, the low state chunks and
             # the current state dictionaries.
