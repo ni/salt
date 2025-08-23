82b59adef9a584e3dd7f8b74b2fafc0eefc5cd5c
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-08-23 14:47:23 -0700

    Clean up tranport closing

--- tests/pytests/functional/transport/tcp/test_pub_server.py
+++ tests/pytests/functional/transport/tcp/test_pub_server.py
@@ -1,4 +1,4 @@
-import threading
+import multiprocessing
 import time
 
 import salt.ext.tornado.gen
@@ -31,11 +31,11 @@ async def test_pub_channel(master_opts, minion_opts, io_loop):
         print("ON RECV")
         publishes.append(message)
 
-    thread = threading.Thread(
+    proc = multiprocessing.Process(
         target=server.publish_daemon,
         args=(publish_payload, presence_callback, remove_presence_callback),
     )
-    thread.start()
+    proc.start()
 
     # Wait for socket to bind.
     time.sleep(3)
@@ -55,4 +55,5 @@ async def test_pub_channel(master_opts, minion_opts, io_loop):
     finally:
         client.close()
         server.close()
-        thread.join()
+        proc.terminate()
+        proc.join()
